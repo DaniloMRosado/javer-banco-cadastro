e5833b03855692996cab152250b1be5430c033d1
DaniloMRosado <rosadomdanilo@gmail.com>
2024-07-30 16:00:11 -0300

    update properties

--- src/main/java/br/com/consulting_delivery/data_manager/DataManagerApplication.java
+++ src/main/java/br/com/consulting_delivery/data_manager/DataManagerApplication.java
@@ -1,7 +1,9 @@
 package br.com.consulting_delivery.data_manager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataManagerApplication {
